83ed1686a8608770e02bec689ac5763dfbce582e
shariqsaleem27 <shariq.saleem06@gmail.com>
2024-01-01 03:59:56 +1100

    practice

---
+++ components/aboutus/Aboutusinfo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Aboutusinfo =() =>{
+return(
+    <div className= "w-[100%] h-screen items-center flex justify-center items-center">
+      <div className='w-[90%]  flex flex-row '>
+        {/* friest half */}
+        <div className="w-[50%] flex " >
+          <img  src="/aboutus/car1.png" />
+        </div>
+        {/* 2nd half */}
+        <div className =" w-[50%] ">
+        <div>
+        about us
+        </div>
+        <div>
+          Our experienced technicians provide reliable and consistent service for all your auto repair needs.Uber is committing to becoming a fully electric, zero-emission platform by 2040, with 100% of rides taking place in zero-emission vehicles, on public transit, or with micromobility. It is our responsibility as the largest mobility platform in the world to more aggressively tackle the challenge of climate change. We will do this by offering riders more ways to ride green, helping drivers go electric, making 
+        </div>
+
+        </div>
+          
+        
+
+      </div>
+  
+    </div>
+)
+
+}
+
+
+export default Aboutusinfo;
---
+++ pages/aboutus.js
@@ -0,0 +1,12 @@
+import Aboutusinfo from "@/components/aboutus/Aboutusinfo";
+import Navbar from "@/components/layout/Navbar";
+import React from "react";
+
+export default function aboutus(){
+return(
+<div>
+    <Navbar/>
+    <Aboutusinfo/>
+</div>
+)
+}
